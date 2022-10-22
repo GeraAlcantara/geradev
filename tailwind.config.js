bb9c17db99ed1f41fcdf9fffa41e89ab0d8b6f7f
Gerardo Alcantara <32178037+GeraAlcantara@users.noreply.github.com>
2022-10-22 01:13:24 -0500

    adding google fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        secondary: "#1070e5",
+        accent: "#00fb15",
+        bg_primary: "#001322",
+      },
+      fontFamily: {
+        Raleway: ["Raleway", ...defaultTheme.fontFamily.sans],
+        Urbanist: ["Urbanist", ...defaultTheme.fontFamily.sans],
+        sans: ["Lato", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };

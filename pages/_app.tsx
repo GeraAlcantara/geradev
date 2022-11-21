bac7e1240d914aaee66c91a663b3710ffe8f5552
Gerardo Alcantara <32178037+GeraAlcantara@users.noreply.github.com>
2022-11-20 19:15:21 -0500

    trying to add ts

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import Layout from "../components/Layout";
+import "../styles/globals.scss";
+import { AppProps } from "next/app";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
+
+export default MyApp;

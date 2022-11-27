f6156e6bfc8d478598b980b1995108be0a7bd771
Gerardo Alcantara <32178037+GeraAlcantara@users.noreply.github.com>
2022-11-27 15:07:15 -0500

    cambiando origen a url live

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const nextConfig = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Origin", value: "https://geradev.vercel.app" },
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           {
             key: "Access-Control-Allow-Headers",

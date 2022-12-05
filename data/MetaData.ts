4c0f5118c3b4064557afabce33c3f76af976845d
Gerardo Alcantara <32178037+GeraAlcantara@users.noreply.github.com>
2022-12-04 21:04:16 -0500

    change headmeta to jaguar ek and es mx

--- data/MetaData.ts
+++ data/MetaData.ts
@@ -1,34 +1,24 @@
-export const MetaData = [
-  {
-    home: {
-      title: "My App",
-      description: "My App Description",
-      keywords: "My App Keywords",
-      author: "My App Author",
-    },
-  },
-];
-
 export const DefaultMetaData = {
   twitterHandle: "@geralcantara79",
-  siteName: "GeraDev",
+  siteName: "Jaguar Ek",
   currentURL: "https://geradev.vercel.app/",
 };
 
 export const MetaDataHome = {
-  pageTitle: "Gerardo Alcantara | Front End Dev",
+  pageTitle: "Jaguar Ek | Front End Dev",
   pageDescription: "Sitio Personal de Gerardo Alcantara donde encontraras experimentos y diseños fuera de la norma en el desarrollo web.",
   socialImage: "/contactsocialog.jpg",
   socialImageAlt: "imagen social del sitio web TODO",
 };
 export const MetaDataContact = {
-  pageTitle: "Contacto | Gerardo Alcantara",
-  pageDescription: "Contact me for any questions or suggestions you may have and while you do it, you´ll be surprised with the coolest captcha on the web.",
+  pageTitle: "Contacto | Jaguar Ek",
+  pageDescription:
+    "Ponte en contacto conmigo para cualquier duda o sugerencia que tengas y mientras lo haces te sorprenderás con el captcha más chidito de la web.",
   socialImage: "/contactsocialog.jpg",
-  socialImageAlt: "An captcha illustration no sutible for readers sorry :(",
+  socialImageAlt: "Una illustracíon de una imagen captcha",
 };
 export const MetaDataBlog = {
-  pageTitle: "Blog | Gerardo Alcantara",
+  pageTitle: "Blog | Jaguar Ek",
   pageDescription: "Blog de Gerardo Alcantara donde encontraras recursos, noticias y articulos de interes. ",
   socialImage: "/contactsocialog.jpg",
   socialImageAlt: "imagen social del sitio web TODO",

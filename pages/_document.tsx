e7a2b87f95b12d01bc8ab12b130afca256fb20b1
Gerardo Alcantara <32178037+GeraAlcantara@users.noreply.github.com>
2023-06-30 11:33:53 -0500

    adding analitic google

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,29 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 const year = new Date().getFullYear()
 
 export default function Document(): JSX.Element {
   return (
     <Html lang="es">
       <Head>
+        <Script
+          id="google-tag-manager"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script
+          dangerouslySetInnerHTML={{
+            __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
+              `
+          }}
+          id="google-tag-manager-data"
+          strategy="afterInteractive"
+        />
         <meta lang="es" />
         <meta content="es" httpEquiv="Content-Language" />
         <meta content={`Copyright ${year} by Gerardo Alcántara`} name="copyright" />

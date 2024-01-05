1ad1302a69ef78bf7371b10cf79dc682937da76b
Gerardo Alcantara <32178037+GeraAlcantara@users.noreply.github.com>
2024-01-04 19:28:38 -0500

    remove analitics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,24 +6,6 @@ export default function Document(): JSX.Element {
   return (
     <Html lang="es">
       <Head>
-        <Script
-          id="google-tag-manager"
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
-          strategy="afterInteractive"
-        />
-
-        <Script
-          dangerouslySetInnerHTML={{
-            __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
-              `
-          }}
-          id="google-tag-manager-data"
-          strategy="afterInteractive"
-        />
         <meta lang="es" />
         <meta content="es" httpEquiv="Content-Language" />
         <meta content={`Copyright ${year} by Gerardo Alcántara`} name="copyright" />
